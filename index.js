1e93c4fed4ea7e1a9f77afaf7ffc28bc7612f8b1
IrfSuar <irving.dev@icloud.com>
2020-02-01 11:19:48 -0600

    hehehehe

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ const { logger } = require('./src/Lib/winston')
 const dbConnect = require('./src/lib/db')
 const server = require('./src/server')
 
+let serve = {}
+
 const listenServer = function() {
     return new Promise((resolve, reject) => {
-        server.listen(8080, () => {
+        let serv = server.listen(8080, () => {
             resolve()
+            console.log(serv)
         })
     })
 }
@@ -28,3 +31,6 @@ main()
         console.error('ERROR', error)
         logger.error('Something wrong', error)
     })
+
+console.log(serv)
+module.exports = serve
--- src/test/Lib/lib.bcrypt.test.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('../../Lib/bcrypt')
-
-describe('bcrypt.hased', () => {
-    it('BCrypt- should return an hashed array from the plainedString', async() => {
-        const plainedText = 'holacomoestas'
-        const hashedString = await bcrypt.hash(plainedText)
-        expect(hashedString).not.toBe('holacomoestas')
-    })
-})
