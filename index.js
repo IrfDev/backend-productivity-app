85a61d4c9938ceb4cbe2e44de72bc86029d7eac2
Irving <irving.dev@icloud.om>
2020-02-01 22:50:21 -0600

    Stop exporting index module for testing

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ const { logger } = require('./src/Lib/winston')
 const dbConnect = require('./src/lib/db')
 const server = require('./src/server')
 
-let serve = {}
+var serve
 
 const listenServer = function() {
     return new Promise((resolve, reject) => {
-        let serv = server.listen(8080, () => {
+        server.listen(8080, () => {
             resolve()
-            console.log(serv)
         })
     })
 }
@@ -31,6 +30,3 @@ main()
         console.error('ERROR', error)
         logger.error('Something wrong', error)
     })
-
-console.log(serv)
-module.exports = serve
